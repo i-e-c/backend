bacae71fc7374f9b31341a4e791aec77853e8da1
Chrysanthe <eloie908@gmail.com>
2022-02-01 17:38:06 +0200

    Adding a homepage

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/authentication");
@@ -7,8 +7,8 @@ const userRoute = require("./routes/users");
 const articleRoute = require("./routes/articles");
 const cors = require("cors")
 const PORT = process.env.PORT || 5000
+
 dotenv.config();
-const app = express();
 app.use(cors())
 app.use(express.json());
 
@@ -21,11 +21,16 @@ mongoose.connect(process.env.db_url, {
 .then(console.log("Connected to MongoDB"))
 .catch((err) => console.log(err));
 
+
 app.use("/route/auth", authRoute);
 app.use("/route/user", userRoute);
 app.use("/route/article", articleRoute);
+app.get("/", (req, res) => {
+    return res.send("Welcome to my api");
+}
+)
 
 app.listen(PORT, () => {
-    
+
     console.log("It is working");
 })
